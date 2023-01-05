8774a89a6b635d9f227407b7b40c4e92c6d7389d
Vanya Korobeynikov <INKorobeynikov@alfabank.ru>
2023-01-05 11:19:33 +0300

    feat(*): add branch protection action

---
+++ index.js
@@ -0,0 +1,27 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const main = async () => {
+    try {
+        const owner = core.getInput('owner', { required: true });
+        const repo = core.getInput('repo', { required: true });
+        const token = core.getInput('token', { required: true });
+        const octokit = new github.getOctokit(token);
+        await octokit.rest.repos.updateBranchProtection({
+            owner,
+            repo,
+            branch: 'main',
+            required_status_checks: null,
+            enforce_admins: false,
+            required_pull_request_reviews: {
+                required_approving_review_count: 1
+            },
+            restrictions: null,
+        })
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+main();
