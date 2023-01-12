af94630405f83f735935fcc150ebc46cfcfc1de3
Vanya Korobeynikov <INKorobeynikov@alfabank.ru>
2023-01-12 15:23:29 +0300

    feat(*): reset tests

--- dist/index.js
+++ dist/index.js
@@ -9694,30 +9694,24 @@ const main = async () => {
         const token = core.getInput('token', { required: true });
         const octokit = new github.getOctokit(token);
 
-        // await octokit.rest.teams.addOrUpdateRepoPermissionsInOrg({
-        //     org: 'ClevertecTest',
-        //     team_slug: 'Mentors',
-        //     owner,
-        //     repo,
-        //     permission: 'push'
-        // });
-
-        // await octokit.rest.repos.updateBranchProtection({
-        //     owner,
-        //     repo,
-        //     branch: 'main',
-        //     required_status_checks: null,
-        //     enforce_admins: false,
-        //     required_pull_request_reviews: {
-        //         required_approving_review_count: 1
-        //     },
-        //     restrictions: null,
-        // });
-
-        await octokit.rest.repos.addCollaborator({
+        await octokit.rest.teams.addOrUpdateRepoPermissionsInOrg({
+            org: 'ClevertecTest',
+            team_slug: 'Mentors',
             owner,
             repo,
-            username: 'Gaurrus',
+            permission: 'push'
+        });
+
+        await octokit.rest.repos.updateBranchProtection({
+            owner,
+            repo,
+            branch: 'main',
+            required_status_checks: null,
+            enforce_admins: false,
+            required_pull_request_reviews: {
+                required_approving_review_count: 1
+            },
+            restrictions: null,
         });
 
     } catch (error) {
